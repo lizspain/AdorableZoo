852d2cf55add2e4b20658ecb0bb4f94eabf4c91b
lizspain <60554755+lizspain@users.noreply.github.com>
2026-08-10 17:47:13 -0700

    Suriyun Cute Zoo packages added
    Added assets from the Suriyun brand cute zoo Unity store packages to establish 3D models for the core zoo animals with animations and materials.

---
+++ Assets/Suriyun/Addon-PetZoo/_Example/Code/Promoter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Suriyun.PetZoo
+{
+	public class Promoter : MonoBehaviour
+	{
+
+		public void OpenURL(string url) {
+			Application.OpenURL(url);
+		}
+	}
+}
